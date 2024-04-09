60bc5a8c6f652d2ab4477ddfaa1ce8f72b24573f
NoOseGG <hoc751@gmail.com>
2024-04-10 02:09:22 +0300

    changed description title, added logo

--- src/components/Feed/CharacterInfo/CharaterInfo.tsx
+++ src/components/Feed/CharacterInfo/CharaterInfo.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import styles from "./CharacterInfo.module.scss";
 
 const CharaterInfo: React.FC = () => {
-const { id } = useParams();
+  const { id } = useParams();
 
-  return (
-    <div>CharaterInfo {id}</div>
-  )
-}
+  return <div className={styles.container}>CharaterInfo {id}</div>;
+};
 
-export default CharaterInfo
+export default CharaterInfo;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,25 @@ import React from "react";
 
 import styles from "./Header.module.scss";
 
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClickLogo = () => {
+    navigate("/");
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles["inner-container"]}>
+      <div className={styles.innerContainer}>
+        <img
+          src={logo}
+          alt="logo"
+          className={styles.logo}
+          onClick={handleClickLogo}
+        />
         <nav>
           <ul className={styles.menu}>
             <li className={styles.menuItem}>Гланая</li>

737f9c4d3f4fd60fa53ab6deae9071c3acc8437b
NoOseGG <hoc751@gmail.com>
2024-04-12 18:59:03 +0300

    added info in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,27 +9,38 @@ import linkedInIcon from "../../assets/icons/linkenin-icon.svg";
 const Footer: React.FC = () => {
   return (
     <footer className={styles.container}>
-      <ul className={styles.menu}>
-        <li className={styles.menuItem}>
-          <a href="https://vk.com/noosegg" target="_blank">
-            <img className="styles.icon" src={vkIcon} alt="vk-icon" />
+      <div className={styles.innerContainer}>
+        <span className={styles.text}>
+          <a className={styles.link} href="https://rickandmortyapi.com/" target="_blank">
+            Rick and Morty API
           </a>
-        </li>
-        <li className={styles.menuItem}>
-          <a href="https://github.com/NoOseGG" target="_blank">
-            <img className="styles.icon" src={githubIcon} alt="github-icon" />
-          </a>
-        </li>
-        <li className={styles.menuItem}>
-          <a href="https://www.linkedin.com/in/yury-svirydzenka-30a03519a/" target="_blank">
-            <img
-              className="styles.icon"
-              src={linkedInIcon}
-              alt="linkedIn-icon"
-            />
-          </a>
-        </li>
-      </ul>
+        </span>
+        <ul className={styles.menu}>
+          <li className={styles.menuItem}>
+            <a href="https://vk.com/noosegg" target="_blank">
+              <img className="styles.icon" src={vkIcon} alt="vk-icon" />
+            </a>
+          </li>
+          <li className={styles.menuItem}>
+            <a href="https://github.com/NoOseGG" target="_blank">
+              <img className="styles.icon" src={githubIcon} alt="github-icon" />
+            </a>
+          </li>
+          <li className={styles.menuItem}>
+            <a
+              href="https://www.linkedin.com/in/yury-svirydzenka-30a03519a/"
+              target="_blank"
+            >
+              <img
+                className="styles.icon"
+                src={linkedInIcon}
+                alt="linkedIn-icon"
+              />
+            </a>
+          </li>
+        </ul>
+        <span className={styles.text}>(c) 2024</span>
+      </div>
     </footer>
   );
 };

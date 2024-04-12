9cdfe966b029ca683af4ae49f8534148fab67731
NoOse <hoc751@gmail.com>
2024-04-12 04:53:39 +0300

    refactored character info

--- src/components/CharacterInfo/CharaterInfo.tsx
+++ src/components/CharacterInfo/CharaterInfo.tsx
@@ -21,7 +21,11 @@ const CharaterInfo: React.FC = () => {
     <div className={styles.topInfo}>
       <div className={styles.info}>
         <div className={styles.name}>{data?.data.name}</div>
-        <div>{data?.data.species}</div>
+        <div><span className={styles.field}>Species: </span>{data?.data.species}</div>
+        <div><span className={styles.field}>Status: </span>{data?.data.status}</div>
+        <div><span className={styles.field}>Gender: </span>{data?.data.gender}</div>
+        <div><span className={styles.field}>Origin: </span>{data?.data.origin.name}</div>
+        <div><span className={styles.field}>Location: </span>{data?.data.location.name}</div>
       </div>
       <img className={styles.avatar} src={data?.data.image} />
     </div>
